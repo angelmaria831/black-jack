a4182dda98e07dd72d5d3c17b323aa8abcba3cd7
Angel Maria Das <angelmariadas@gmail.com>
2024-09-03 20:01:22 +0100

    test : add unit test for shuffling deck of cards

--- __tests__/Decks.test.tsx
+++ __tests__/Decks.test.tsx
@@ -5,14 +5,32 @@ const {Card} = CardLogic
 
 describe( 'Deck', () => {
 
+    let deck : Deck;
+
+    beforeEach(() => {
+
+        deck = new Deck();
+    })
+
     it('should be defined', () => {
-        const deck = new Deck()
         expect(deck.cards).toBeDefined()
-    });
+    })
 
     it('card in deck should be an instance of Card', () => {
         const deck = new Deck()
         expect(deck.cards[0]).toBeInstanceOf(Card)
         expect(deck.cards.length).toBe(52)
+    })
+
+    it('should rearrange cards in deck when shuffled', () => {
+        //Create shallow copy of deck.cards array.
+        const originalOrder = [...deck.cards];
+        deck.shuffleDeck();
+
+        const shuffledOrder = deck.cards;
+
+        expect(shuffledOrder).not.toEqual(originalOrder)
+        expect(shuffledOrder.length).toBe(52)
+
     })
 })
