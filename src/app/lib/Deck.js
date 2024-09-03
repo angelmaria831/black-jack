c30a7dda5212c9d25f49d47dbd03a5093fff04b3
Angel Maria Das <angelmariadas@gmail.com>
2024-09-03 19:42:59 +0100

    feat : Add shuffle Functionality

--- src/app/lib/Deck.js
+++ src/app/lib/Deck.js
@@ -3,8 +3,21 @@ import CardLogic from './Card'
 const {suits, ranks, Card} = CardLogic
 
 class Deck {
+
     constructor(){
         this.cards = suits.flatMap(suit => ranks.map(rank => new Card(suit, rank)))
+    }
+
+    shuffleDeck() {
+
+        for(let i = this.cards.length - 1; i > 0; i--){
+            const newIndex = Math.floor(Math.random() * (i+1))
+            const newIndexValue = this.cards[newIndex]
+            this.cards[newIndex] = this.cards[i]
+            this.cards[i] = newIndexValue
+        }
+
+        return this.cards
     }
 }
 
