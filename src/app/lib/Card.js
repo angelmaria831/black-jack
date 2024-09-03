18f20c89724b1892feab42ecb429da295a4e7525
Angel Maria Das <angelmariadas@gmail.com>
2024-09-03 19:17:59 +0100

    Create class Card defining suits and ranks

--- src/app/lib/Card.js
+++ src/app/lib/Card.js
@@ -8,4 +8,7 @@ class Card{
 const suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
 const ranks = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
 
-export default Card
+export default {
+    Card,
+    suits,
+    ranks}
