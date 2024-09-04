9b86d87a02760c55d0e80fbadeb846c1429e18ac
Angel Maria Das <angelmariadas@gmail.com>
2024-09-04 21:06:43 +0100

    test : added testcases valid for Deck.ts - Valid initialization for Deck - Check the card in deck is instance of class Card - Check shuflling cards - Check draw cards with valid count of one or two - Manage error while invalid count of cards are drawn

--- __tests__/Decks.test.tsx
+++ __tests__/Decks.test.tsx
@@ -1,7 +1,5 @@
 import Deck from '@/app/lib/Deck'
-import CardLogic from '@/app/lib/Card'
-
-const {Card} = CardLogic
+import { Card } from '@/app/lib/Card'
 
 describe( 'Deck', () => {
 
@@ -13,13 +11,12 @@ describe( 'Deck', () => {
     })
 
     it('should be defined', () => {
-        expect(deck.cards).toBeDefined()
+        expect(deck.cards).toBeDefined();
     })
 
     it('card in deck should be an instance of Card', () => {
-        const deck = new Deck()
-        expect(deck.cards[0]).toBeInstanceOf(Card)
-        expect(deck.cards.length).toBe(52)
+        expect(deck.cards[0]).toBeInstanceOf(Card);
+        expect(deck.cards.length).toBe(52);
     })
 
     it('should rearrange cards in deck when shuffled', () => {
@@ -29,8 +26,43 @@ describe( 'Deck', () => {
 
         const shuffledOrder = deck.cards;
 
-        expect(shuffledOrder).not.toEqual(originalOrder)
-        expect(shuffledOrder.length).toBe(52)
+        expect(shuffledOrder).not.toEqual(originalOrder);
+        expect(shuffledOrder.length).toBe(52);
+    })
 
+    it('should be able to draw one card from deck', () => {
+        const drawnOneCard = deck.draw(1);
+        expect(drawnOneCard.length).toBe(1);
+        expect(deck.cards.length).toBe(51);
+        expect(drawnOneCard[0]).toBeInstanceOf(Card);
+    })
+
+    it('should be able to draw two cards from deck', () => {
+        const drawnTwoCards = deck.draw(2);
+        expect(drawnTwoCards.length).toBe(2);
+        expect(deck.cards.length).toBe(50);
+
+        expect(drawnTwoCards[0]).toBeInstanceOf(Card);
+        expect(drawnTwoCards[1]).toBeInstanceOf(Card);
+    })
+
+    it('should throw error if draws card besides one or two',() => {
+        
+        expect(() => {
+            const drawnZeroCards = deck.draw(0);
+        }).toThrow("Invalid Count to Draw");
+
+        expect(() => {
+            const drawnThreeCards = deck.draw(3);
+        }).toThrow("Invalid Count to Draw");
+
+        expect(() => {
+            const drawnNegativeCards = deck.draw(-1);
+        }).toThrow("Invalid Count to Draw");
+
+        expect(() => {
+            const drawnFiveCards = deck.draw(5);
+        }).toThrow("Invalid Count to Draw")
+        
     })
 })
