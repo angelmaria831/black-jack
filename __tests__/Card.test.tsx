dd5a1be43061091604c5700ab5ec1345ea4fd7f3
Angel Maria Das <angelmariadas@gmail.com>
2024-09-04 21:02:53 +0100

    test : added testcase for verifying cards with all valid suits and ranks

--- __tests__/Card.test.tsx
+++ __tests__/Card.test.tsx
@@ -1,6 +1,4 @@
-import CardLogic from '@/app/lib/Card'
-
-const {Card} = CardLogic
+import { Card, suits, ranks } from '@/app/lib/Card'
 
 describe('Card', () => {
 
@@ -9,5 +7,16 @@ describe('Card', () => {
         const card = new Card('Hearts', 'A')
         expect(card.suit).toBe('Hearts')
         expect(card.rank).toBe('A')
+    })
+
+    it('should create cards with all valid suits and ranks', () => {
+
+        suits.forEach(suit => {
+            ranks.forEach(rank => {
+                const card = new Card(suit, rank);
+                expect(card.suit).toBe(suit);
+                expect(card.rank).toBe(rank);
+            });
+        });
     })
 })
