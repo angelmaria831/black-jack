5a38040f26f7bd7e8594afe9a54f236635a672f2
Angel Maria Das <angelmariadas@gmail.com>
2024-09-06 20:02:30 +0100

    feat : add Player with card handling logic and score calculation
    - Implemented the Player class to manage player name and hand of cards.
    - Added methods to receive cards and calculate total score with ace handling.
    - Ensured proper type safety using Typescript.

---
+++ src/app/lib/Player.ts
@@ -0,0 +1,46 @@
+import { CardType } from "./Card";
+
+class Player {
+
+    name: string;
+    hand: CardType[];
+
+    constructor(name: string) {
+        this.name = name;
+        this.hand = [];
+    }
+
+    receiveCards(cards: CardType[]) {
+
+        if(cards.length === 0) throw new Error("No cards were received");
+        
+        this.hand = [...this.hand, ...cards];
+        return this.hand;
+    }
+
+    totalScore() {
+
+        let total: number  = 0;
+        let containsAce: boolean = false;
+
+        for (const { rank } of this.hand) {
+
+            if(rank === 'A') {
+                total += 11;
+                containsAce = true;
+            } else if(['J', 'Q', 'K'].includes(rank)) {
+                total += 10;
+            } else {
+                total += parseInt(rank);
+            }
+        }
+
+        if(containsAce && total > 21) total -= 10;
+
+        return total;
+
+    }
+
+}
+
+export default Player;
