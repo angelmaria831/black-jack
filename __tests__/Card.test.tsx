240d4eb8d2a179e6da5bcf9bee24c5a6848585bd
Angel Maria Das <angelmariadas@gmail.com>
2024-09-08 14:45:16 +0100

    test : added new test cases - Throw error with invalid ranks or suits. - Throw error with undefined/null ranks or suits.

--- __tests__/Card.test.tsx
+++ __tests__/Card.test.tsx
@@ -9,6 +9,7 @@ describe('Card', () => {
         expect(card.rank).toBe('A')
     })
 
+    
     it('should create cards with all valid suits and ranks', () => {
 
         suits.forEach(suit => {
@@ -19,4 +20,43 @@ describe('Card', () => {
             });
         });
     })
+
+    it('should throw error when card with invalid suits or ranks are defined', () => {
+
+        expect(() => {
+            new Card('A', 'Hearts');
+        }).toThrow('Invalid suit or rank'); // 'A' is a rank, not a suit
+
+        expect(() => {
+            new Card('Diamonds', '12');
+        }).toThrow('Invalid suit or rank'); // Invalid rank
+
+        expect(() => {
+            new Card('Club', '3');
+        }).toThrow('Invalid suit or rank'); // Typo : 'Clubs'
+    })
+
+    it('should throw an error if suit or rank is missing', () => {
+
+        expect(() => {
+            new Card(undefined as any, 'A');
+        }).toThrow('Invalid suit or rank');
+
+        expect(() => {
+            new Card('Spades', undefined as any);
+        }).toThrow('Invalid suit or rank');
+    })
+
+    it('should throw an error if suit or rank is null', () => {
+
+        expect(() => {
+            new Card(null as any, 'A');
+        }).toThrow('Invalid suit or rank');
+
+        expect(() => {
+            new Card('Spades', null as any);
+        }).toThrow('Invalid suit or rank');
+
+    })
+
 })
