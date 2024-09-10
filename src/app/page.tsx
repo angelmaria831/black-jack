4dc889e7be4b55ca7b362b2e91a96940c1501cfe
Angel Maria Das <angelmariadas@gmail.com>
2024-09-10 11:05:36 +0100

    feat: add smooth scroll to sections for 'BlackJack' and 'Rules' in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,22 @@ import React from "react";
 
 export default function Home() {
 
-
+    const scrollToDiv = (divId: string) =>{
+        const section = document.getElementById(divId);
+        section?.scrollIntoView({behavior: 'smooth'})
+    }
+    
     return (
         <div className="relative w-full h-screen">
-            <nav className="fixed top-0 left-0 w-full py-8 px-6 bg-gradient-to-b from-black to-transparent z-20">
+            <nav className="fixed top-0 left-0 w-full py-6 px-6 bg-gradient-to-b from-black to-transparent z-20">
                 <div className="flex justify-between items-center max-w-7xl mx-auto">
-                    <h1 className="text-2xl font-bold text-white">BlackJack</h1>
-                    <span className="text-white text-xl cursor-pointer hover:text-red-400 hover:font-bold transition duration-300">Rules</span>
+                    <h1 className="text-3xl font-bold text-white cursor-pointer hover:text-red-400 hover:font-bold transition duration-300" ><span onClick={() => scrollToDiv("home")}>BlackJack</span></h1>
+                    <span className="text-white text-xl cursor-pointer hover:text-red-400 hover:font-bold transition duration-300"
+                    onClick={() =>scrollToDiv("rules")}>Rules</span>
                 </div>
             </nav>
 
-            <main className="relative w-full h-screen overflow-hidden">
+            <main className="relative w-full h-screen overflow-hidden" id="home">
                 <video
                     autoPlay
                     loop
