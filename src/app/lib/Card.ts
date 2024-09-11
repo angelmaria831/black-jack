64895a58360f861ab673a6dea2e6ad500e2e8f20
Angel Maria Das <angelmariadas@gmail.com>
2024-09-11 12:56:24 +0100

    feat: add card images and getCardImagePath() in Vard.ts

--- src/app/lib/Card.ts
+++ src/app/lib/Card.ts
@@ -1,4 +1,4 @@
-const suits: Set<string> = new Set(["Hearts", "Diamonds", "Clubs", "Spades"]);
+const suits: Set<string> = new Set(['Hearts', 'Diamonds', 'Clubs', 'Spades']);
 const ranks: Set<string> = new Set(['A','2','3','4','5','6','7','8','9','10','J','Q','K']);
 
 class Card{
@@ -7,18 +7,26 @@ class Card{
 
     constructor(suit : string, rank : string){
         if(!suits.has(suit) || !ranks.has(rank))
-            throw new Error("Invalid suit or rank");
+            throw new Error('Invalid suit or rank');
         this.suit = suit;
         this.rank = rank;
     }
+
 }
 
 const suitsArray: string[] = Array.from(suits);
 const ranksArray: string[] = Array.from(ranks);
+type CardType = Card;
+
+const getCardImagePath = (card: CardType): string =>{
+    return `/${card.suit.toLowerCase()}_${card.rank.toLowerCase()}.png`;
+}
 
 export {
     Card,
     suitsArray as suits,
-    ranksArray as ranks };
+    ranksArray as ranks,
+    getCardImagePath
+ };
 
-export type CardType = Card;
+ export type { CardType };
