372556b1e2b8779cbb914d183b806a60b418f9af
Angel Maria Das <angelmariadas@gmail.com>
2024-09-26 19:05:26 +0100

    feat: new feature method to hit new card - Update type initializing Deck - Update in return Obj from showCardsImage, include keys card,flipped

--- src/app/lib/Game.ts
+++ src/app/lib/Game.ts
@@ -6,20 +6,21 @@ class Game {
 
     player: PlayerType;
     dealer: PlayerType;
-    deck: DeckType | null;
+    deck: DeckType;
     isDealerCardHidden: boolean;
+    winner: string;
 
     constructor(playerName: string) {
         this.player = new Player(playerName.toLowerCase());
         this.dealer = new Player("dealer");
-        this.deck = null;
+        this.deck = new Deck();
         this.isDealerCardHidden = true;
+        this.winner = '';
     }
 
 
     startGame() {
 
-        this.deck = new Deck(); 
         this.deck.shuffleDeck();
         
         this.player.receiveCards(this.deck.draw(2));
@@ -29,16 +30,36 @@ class Game {
 
     showCardsImage() {
         return {
-            dealerCards: this.dealer.hand.map((card, index) => (this.isDealerCardHidden && index === 0) ? cardBackImagePath : getCardImagePath(card)),
-            playerCards: this.player.hand.map(card => getCardImagePath(card))
+            dealerCards: this.dealer.hand.map((card, index) => (this.isDealerCardHidden && index === 0) ? 
+            {'card': cardBackImagePath, 'flipped':true } : 
+            {'card': getCardImagePath(card), 'flipped': false}),
+
+            playerCards: this.player.hand.map((card) => ({'card': getCardImagePath(card), 'flipped': false}))
         }
     } 
 
-    getTotalScore() {
-        return {
+    getTotalScoreAndStatus() {
+
+        const scores = {
             dealerScore: this.dealer.totalScore(this.isDealerCardHidden),
-            playerScore: this.player.totalScore()
+            playerScore: this.player.totalScore(),
+            isGameOver: false
         }
+
+        if(scores.playerScore === 21 || scores.dealerScore > 21){
+            scores.isGameOver = true;
+            this.winner = 'player';
+        }else if(scores.dealerScore === 21 || scores.playerScore > 21){
+            scores.isGameOver = true;
+            this.winner = 'dealer';
+        }
+        return scores;
+    }
+
+    hitWithCard() {
+        const card = this.deck.draw(1)
+        this.player.receiveCards(card);
+        return {'card': getCardImagePath(card[0]), flipped: false}
     }
 }
 
